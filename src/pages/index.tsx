35f1cbf3f8c5ae26c8f993fe0cb9fc211add5098
imanmaris99 <imanmaris99@yahoo.com>
2024-01-22 06:04:52 +0700

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,33 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useContext } from 'react'
-import { AppContext } from '@/providers/AppProvider'
+import Layout from "@/layouts";
+import Head from "next/head";
+import { Card } from "@/components";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const context = useContext(AppContext);
-
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
+
+    <main 
+    className={`bg-sky-50/[.9] rounded-b-xl flex min-h-screen flex-col items-center justify-center`}
     >
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-      <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Halaman Depan {' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <Layout >
+        <section className={`bg-sky-100/[1] rounded-b-sm p-24`}>
+          <Head>
+            <title>{'Halaman Index'}</title>
+          </Head>
 
-            {context?.forecast?.date}
-          </h2>
-        </a>
+          <Card>
+            <Link passHref href={'/weather/register'} className={`mb-3 text-2xl font-semibold`}>
+              Silahkan register dahulu {' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </Link>
+          </Card>
+        </section>
+      </Layout>
+    </main>  
 
-      </div>
-
-    </main>
   )
 }
