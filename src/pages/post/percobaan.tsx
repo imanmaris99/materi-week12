00838c2ad8b2dfa493f22a71ffbc35c21bf9e56a
imanmaris99 <imanmaris99@yahoo.com>
2024-01-22 06:19:43 +0700

    third commited

--- src/pages/post/percobaan.tsx
+++ src/pages/post/percobaan.tsx
@@ -1,9 +1,9 @@
-interface Post {
-    title: string;
-  }
-  
+import { OldCard } from "@/components";
+import Layout from "@/layouts";
+
   interface Resulted {
     name: string;
+    url:string;
   }
   
   interface ListPokemon {
@@ -11,33 +11,48 @@ interface Post {
   }
   
   interface Props {
-    posts: Post[];
     results: ListPokemon;
   }
   
   const CobaPost = ({ results }: Props) => {
     return (
-      <main className={`flex min-h-screen flex-col items-center justify-center p-24`}>
-        <div>
-          <h2 className="text-lg font-bold p-12">
-            {'Halaman Post dengan Server Side Fetch '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-  
-          {results.results.map((result, index) => (
-            <p key={index}>{result.name}</p>
-          ))}
-        </div>
+      
+      <main 
+      className={`bg-sky-50/[.9] rounded-b-xl flex min-h-screen flex-col items-center justify-center`}
+      >
+        <Layout>
+          <OldCard direction="column">
+            <section className="w-full p-5 bg-sky-100/[.9] text-black flex flex-col justify-center rounded-md">
+              <h2 className="w-full p-5 text-3xl bg-sky-100/[.9] text-black flex justify-center rounded-md">
+                {'Halaman Pokemon Post'}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+            <OldCard direction="column">
+              {results.results.map((result, index) => (
+                
+                <OldCard key={index} direction="column" display="flex" justifyContent="center">
+                  <p key={index}>{result.name}</p>
+                  <p key={index}>{result.url}</p>               
+                </OldCard>
+
+              ))}
+            </OldCard>
+
+            </section>
+          </OldCard>
+        </Layout>
+
       </main>
     );
   };
   
   export const getServerSideProps = async () => {
-    const responseKedua = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30');
+    const responseKedua = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15');
     const timedata: ListPokemon = await responseKedua.json();
-  
+    console.log (timedata);
+
     return {
       props: {
         results: timedata,
