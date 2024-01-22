3c931c656e6cc72bf9bb50db9b91f7ea9af947ed
imanmaris99 <imanmaris99@yahoo.com>
2024-01-22 19:10:10 +0700

    perbaikan penambahan policy masuk page

--- src/pages/weather/weatherfinder.tsx
+++ src/pages/weather/weatherfinder.tsx
@@ -9,7 +9,7 @@ import humidity_icon from '../../components/Assets/humidity.png';
 import angin_icon from '../../components/Assets/angin.png';
 import panas_icon from '../../components/Assets/panas.png';
 import Image from "next/image";
-import { useRouter } from "next/router";
+import Register from "./register";
 
 interface LocationData {
   lat: number;
@@ -107,86 +107,70 @@ const WeatherFinder = () => {
     fetchData();
   }, []); // Empty dependency array ensures that this effect runs only once on mount
 
-  const router = useRouter();
-  const token = localStorage.getItem("customToken");
-  if (token) {
-    return (
-    
-        <main className={`bg-sky-50/[.9] rounded-b-xl flex min-h-screen flex-col items-center justify-center`}>
-          <Layout>
-            <OldCard direction="column">
-              <section className="w-full p-5 backdrop-blur-xl bg-sky-100/[.9] text-black flex flex-row justify-center item-center">
-                <input
-                  type="text"
-                  className="cityInput rounded-l-2xl pl-3 pt-2 pb-2"
-                  placeholder="Search Location"
-                  ref={searchRef} />
-                <div className="search-icon bg-sky-50/[.9] rounded-r-2xl pl-2 pr-2 pt-2 pb-2" onClick={onSearch}>
-                  <Image className="h-7 w-7 cursor-pointer" src={search_icon} alt="search logo" />
-                </div>
-              </section>
-    
-              <section className='backdrop-blur-xl bg-sky-100/[.9] rounded-b-xl p-10'>
-                {data.map((item, index) => (
-                    <section key={index} className="flex flex-wrap flex-col items-center">
-                        <h2 className='font-bold text-2xl text-slate-500 pb-2'>{item.location.country}</h2>
-                        <h3 className='font-bold text-l text-slate-500 pb-2'>{item.location.tz_id}</h3>
-                        <p>{item.location.localtime}</p>
-    
-                        <img
-                        className="m-5 bg-violet-50 hover:bg-violet-200 active:bg-violet-700 rounded-full p-5"
-                        src={item.current.condition.icon}
-                        alt="logo cuaca"
-                        />
-                            
-                        <p className='flex justify-center text-2xl pb-2 items-center'><Image className='h-8 w-auto' src={suhu_max} alt="logo suhu udara" /> {item.current.temp_c+"°C"}</p>
-    
-                        <p className="description">{"Condition :"+ item.current.condition.text}</p>
-    
-                    </section>
-                ))}
-              </section>
-    
-              <section className='backdrop-blur-xl bg-sky-100/[.9] rounded-b-xl p-10'>
-                {data.map((item, index) => (
-    
-                    <section key={index} className='flex flex-row gap-1 justify-center items-center '>
-    
-                        <div className='flex flex-col gap-2 p-2 justify-center items-center'>
-                            <p className='flex flex-col justify-center text-l items-center'><Image className='h-8 w-auto' src={humidity_icon} alt="logo kelembaban udara" />{item.current.humidity+"%"}</p>
-                            <p className='flex justify-center text-sm items-center'>{'Kelembapan %'}</p>
-                        </div>
-    
-                        <div className='flex flex-col gap-2 p-2 justify-center items-center'>
-                            <p className='flex flex-col justify-center text-l items-center'><Image className='h-8 w-auto' src={angin_icon} alt="logo kecepatan udara" />{item.current.wind_mph+"m/s"}</p>
-                            <p className='flex justify-center text-sm items-center'>{'Laju Angin m/s'}</p>
-                        </div>
-    
-                        <div className='flex flex-col gap-2 p-2 justify-center items-center'>
-                            <p className='flex flex-col justify-center text-l items-center'><Image className='h-8 w-auto' src={panas_icon} alt="logo kecepatan udara" />{item.current.pressure_mb+"hPa"}</p>
-                            <p className='flex justify-center text-sm items-center'>{'Tekanan hPa'}</p>
-                        </div>
-                    </section>
-    
-                ))}
-              </section>
-    
-            </OldCard>
-          </Layout>
-        </main>
-      );
-  }
-
   return (
-    <OldCard direction="row" justifyContent="center">
-      <button className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-lg m-2 px-3 py-2 text-2xl font-medium" type="button" onClick={() => router.push('/weather/register')}>
-        Register dulu untuk dapat token !!!
-      </button>
-    </OldCard>
+    <main className={`bg-sky-50/[.9] rounded-b-xl flex min-h-screen flex-col items-center justify-center`}>
+      <Layout>
+        <OldCard direction="column">
+          <section className="w-full p-5 backdrop-blur-xl bg-sky-100/[.9] text-black flex flex-row justify-center item-center">
+            <input
+              type="text"
+              className="cityInput rounded-l-2xl pl-3 pt-2 pb-2"
+              placeholder="Search Location"
+              ref={searchRef} />
+            <div className="search-icon bg-sky-50/[.9] rounded-r-2xl pl-2 pr-2 pt-2 pb-2" onClick={onSearch}>
+              <Image className="h-7 w-7 cursor-pointer" src={search_icon} alt="search logo" />
+            </div>
+          </section>
 
-  )  
+          <section className='backdrop-blur-xl bg-sky-100/[.9] rounded-b-xl p-10'>
+            {data.map((item, index) => (
+                <section key={index} className="flex flex-wrap flex-col items-center">
+                    <h2 className='font-bold text-2xl text-slate-500 pb-2'>{item.location.country}</h2>
+                    <h3 className='font-bold text-l text-slate-500 pb-2'>{item.location.tz_id}</h3>
+                    <p>{item.location.localtime}</p>
 
-  
+                    <img
+                    className="m-5 bg-violet-50 hover:bg-violet-200 active:bg-violet-700 rounded-full p-5"
+                    src={item.current.condition.icon}
+                    alt="logo cuaca"
+                    />
+                        
+                    <p className='flex justify-center text-2xl pb-2 items-center'><Image className='h-8 w-auto' src={suhu_max} alt="logo suhu udara" /> {item.current.temp_c+"°C"}</p>
+
+                    <p className="description">{"Condition :"+ item.current.condition.text}</p>
+
+                </section>
+            ))}
+          </section>
+
+          <section className='backdrop-blur-xl bg-sky-100/[.9] rounded-b-xl p-10'>
+            {data.map((item, index) => (
+
+                <section key={index} className='flex flex-row gap-1 justify-center items-center '>
+
+                    <div className='flex flex-col gap-2 p-2 justify-center items-center'>
+                        <p className='flex flex-col justify-center text-l items-center'><Image className='h-8 w-auto' src={humidity_icon} alt="logo kelembaban udara" />{item.current.humidity+"%"}</p>
+                        <p className='flex justify-center text-sm items-center'>{'Kelembapan %'}</p>
+                    </div>
+
+                    <div className='flex flex-col gap-2 p-2 justify-center items-center'>
+                        <p className='flex flex-col justify-center text-l items-center'><Image className='h-8 w-auto' src={angin_icon} alt="logo kecepatan udara" />{item.current.wind_mph+"m/s"}</p>
+                        <p className='flex justify-center text-sm items-center'>{'Laju Angin m/s'}</p>
+                    </div>
+
+                    <div className='flex flex-col gap-2 p-2 justify-center items-center'>
+                        <p className='flex flex-col justify-center text-l items-center'><Image className='h-8 w-auto' src={panas_icon} alt="logo kecepatan udara" />{item.current.pressure_mb+"hPa"}</p>
+                        <p className='flex justify-center text-sm items-center'>{'Tekanan hPa'}</p>
+                    </div>
+                </section>
+
+            ))}
+          </section>
+
+        </OldCard>
+      </Layout>
+    </main>
+  );
 };
 
 export default WeatherFinder;
